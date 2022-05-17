de0f95860113777e5dabe49c39e3c0c065ddc67e
MAICON FERREIRA DA SILVA <maicon.ferreira@univille.br>
2022-05-17 20:38:47 -0300

    Project Update

--- src/main/java/br/univille/dsi2022/entity/Dispensa.java
+++ src/main/java/br/univille/dsi2022/entity/Dispensa.java
@@ -1,24 +1,22 @@
 package br.univille.dsi2022.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Dispensa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private SetorDaDispensa setor;
-
-    public SetorDaDispensa getSetor() {
-        return setor;
-    }
-
-    public void setSetor(SetorDaDispensa setor) {
-        this.setor = setor;
-    }
-
-    
+    @OneToMany(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "setor_id")
+    private List<SetorDaDispensa> setor = new ArrayList<>();
 }
--- src/main/java/br/univille/dsi2022/entity/ListaDeCompras.java
+++ src/main/java/br/univille/dsi2022/entity/ListaDeCompras.java
@@ -1,28 +1,35 @@
 package br.univille.dsi2022.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ListaDeCompras {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private ItemDaLista item;
+    @OneToMany(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "item_id")
+    private List<ItemDaLista> item = new ArrayList<>();
     public long getId() {
         return id;
     }
     public void setId(long id) {
         this.id = id;
     }
-    public ItemDaLista getItem() {
+    public List<ItemDaLista> getItem() {
         return item;
     }
-    public void setItem(ItemDaLista item) {
+    public void setItem(List<ItemDaLista> item) {
         this.item = item;
     }
-
     
 }
