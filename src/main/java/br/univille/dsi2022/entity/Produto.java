a363ca3e0cc97a23161030b48ded8290fc293f1f
MAICON FERREIRA DA SILVA <maicon.ferreira@univille.br>
2022-07-04 22:33:27 -0300

    corrigido alterar produto

--- src/main/java/br/univille/dsi2022/entity/Produto.java
+++ src/main/java/br/univille/dsi2022/entity/Produto.java
@@ -14,35 +14,45 @@ public class Produto {
     private int quantidadeAtual;
     private int quantidadeIdeal;
     private int quantidadeMinima;
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
-    public String getNome() {
+
+    public String getProdutoNome() {
         return produtoNome;
     }
-    public void setNome(String produtoNome) {
+
+    public void setProdutoNome(String produtoNome) {
         this.produtoNome = produtoNome;
     }
+
     public int getQuantidadeAtual() {
         return quantidadeAtual;
     }
+
     public void setQuantidadeAtual(int quantidadeAtual) {
         this.quantidadeAtual = quantidadeAtual;
     }
+
     public int getQuantidadeIdeal() {
         return quantidadeIdeal;
     }
+
     public void setQuantidadeIdeal(int quantidadeIdeal) {
         this.quantidadeIdeal = quantidadeIdeal;
     }
+
     public int getQuantidadeMinima() {
         return quantidadeMinima;
     }
+
     public void setQuantidadeMinima(int quantidadeMinima) {
         this.quantidadeMinima = quantidadeMinima;
     }
-    
+
 }
